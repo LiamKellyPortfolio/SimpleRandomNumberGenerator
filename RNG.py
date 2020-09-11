2e87ed76f411a39867efa74ba7f6f0d1b3b19498
LiamKellyPortfolio <71149221+LiamKellyPortfolio@users.noreply.github.com>
2020-09-11 14:29:48 -0400

    Add files via upload

---
+++ RNG.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+#!/usr/bin/env python
+
+import Random
+import time
+
+os.system('clear')
+print "+ --------------------------------------------- +"
+time.sleep(0.4)
+print  "|----- Welcome! -----+"
+time.sleep(0.9)
+print "|----- My name is Liam! -----|" 
+time.sleep(0.4)
+print "|-------- This is a Random Number Generator"
+time.sleep(0.2)
+def rngmain():
+    while True:
+        try:
+            elif main == "Random Number":
+                print(random.randrange(100, 10, 2))
+		rngmain()
+rngmain()
